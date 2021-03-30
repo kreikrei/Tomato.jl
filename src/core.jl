50a2818e774784d36aa1b878e346ec5cb280e1ee
kreikrei <kreitonstrs@gmail.com>
2021-03-30 18:23:53 +0700

    disaggr function

--- src/core.jl
+++ src/core.jl
@@ -70,9 +70,79 @@ function dis(R::Dict,j_dis::Int64,k_dis::Int64,t_dis::Int64)
     #CREATE DISAGGREGATED INDEX
     dis_idx = [j_dis]
     for i in K(k_dis).cover
-        if value.(deli[(k_dis,t_dis)].y[i,j_dis]) > 0
+        if value.(R[(k_dis,t_dis)].y[i,j_dis]) > 0
             push!(dis_idx,i)
         end
     end
-    
+
+    #CREATE AGGREGATE GUIDE (FOR CONSTRAINTS)
+    v_guide = Dict{Int64,Int64}(dis_idx .=> [0 for i in dis_idx])
+    v_guide[j_dis] = round(value(R[(k_dis,t_dis)].v[j_dis]))
+    u_guide = Dict{Int64,Int64}(
+        dis_idx .=> [round(value(R[(k_dis,t_dis)].u[i,j_dis])) for i in dis_idx]
+    )
+    y_guide = Dict{Int64,Int64}(
+        dis_idx .=> [round(value(R[(k_dis,t_dis)].y[i,j_dis])) for i in dis_idx]
+    )
+
+    #CREATE MANIFESTS OF DISAGGREGATION
+    M = collect(1:Int64(value(R[(k_dis,t_dis)].z[j_dis])))
+
+    #CREATE MODEL
+    feas = Model(get_optimizer())
+
+    #CREATE DELIVERY VARIABLES AND CONSTRAINTS
+    R = Dict()
+
+    for m in M
+        z = @variable(feas, [j = dis_idx], Bin)
+        y = @variable(feas, [i = dis_idx], Bin)
+        x = @variable(feas, [i = dis_idx, j = dis_idx], Bin)
+        v = @variable(feas, [j = dis_idx], Int)
+        u = @variable(feas, [i = dis_idx], Int)
+        w = @variable(feas, [i = dis_idx, j = dis_idx], Int)
+
+        @constraint(feas, [j = dis_idx], v[j] >= 0)
+        @constraint(feas, [i = dis_idx], u[i] >= 0)
+        @constraint(feas, [i = dis_idx, j = dis_idx], w[i,j] >= 0)
+
+        @constraint(feas, sum(u[i] for i in dis_idx) - v[j_dis] == 0)
+        @constraint(feas, [i = dis_idx],
+            sum(w[j,i] for j in dis_idx) - sum(w[i,j] for j in dis_idx) == u[i] - v[i]
+        )
+        @constraint(feas, [i = dis_idx],
+            sum(x[j,i] for j in dis_idx) - sum(x[i,j] for j in dis_idx) == 0
+        )
+
+        @constraint(feas, [i = dis_idx], u[i] <= K(k_dis).Q * y[i])
+        @constraint(feas, v[j_dis] <= K(k_dis).Q * z[j_dis])
+        @constraint(feas, [i = dis_idx, j = dis_idx], w[i,j] <= K(k_dis).Q * x[i,j])
+
+        R[m] = disaggr(u,v,w,y,z,x)
+    end
+
+    #AGGREGATE LIMIT CONSTRAINTS
+    @constraint(feas, [j = dis_idx], sum(R[m].v[j] for m in M) == v_guide[j])
+    @constraint(feas, [i = dis_idx], sum(R[m].u[i] for m in M) == u_guide[i])
+    @constraint(feas, [i = dis_idx], sum(R[m].y[i] for m in M) == y_guide[i])
+
+    #OBJECTIVE FUNCTION
+    @objective(feas, Min,
+        sum(K(k_dis).vx * dist(i,j) * R[m].x[i,j]
+            for i in dis_idx, j in dis_idx, m in M
+        ) +
+        sum(K(k_dis).vl * dist(i,j) * R[m].w[i,j]
+            for i in dis_idx, j in dis_idx, m in M
+        ) +
+        sum(K(k_dis).fd * R[m].u[i]
+            for i in dis_idx, m in M
+        ) +
+        sum(K(k_dis).fp * R[m].z[j]
+            for j in dis_idx, m in M
+        )
+    )
+
+    optimize!(feas)
+
+    return feas,R
 end
